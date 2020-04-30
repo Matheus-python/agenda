ca0e4f073dfe98bda7985b3a3a46fea372bd4096
Matheus-python <ortiz.matheus2017@gmail.com>
2020-04-30 10:40:29 -0300

    Alterar o settings debug False, Timezone e resposta com json.

--- core/views.py
+++ core/views.py
@@ -3,25 +3,33 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime
+from django.http.response import Http404, JsonResponse
+
 
 # Create your views/controllers here.
 
-#def index(request):
-  #  return redirect('/agenda/')
+# def index(request):
+#  return redirect('/agenda/')
 
 @login_required(login_url='/login/')
 def lista_eventos(request):
+    data_atual = datetime.now()
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
 
 def login_user(request):
     return render(request, 'login.html')
 
+
 def logout_user(request):
     logout(request)
     return redirect('/')
+
 
 def submit_login(request):
     if request.POST:
@@ -35,6 +43,7 @@ def submit_login(request):
             messages.error(request, 'Usuário ou senha inválidos')
             return redirect('/')
 
+
 @login_required(login_url='/login/')
 def evento(request):
     id_evento = request.GET.get('id')
@@ -44,6 +53,7 @@ def evento(request):
         print('evento_titulo')
     return render(request, 'evento.html', dados)
 
+
 @login_required(login_url='/login/')
 def submit_evento(request):
     if request.POST:
@@ -51,34 +61,47 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
+        # Pego o id_evento
         id_evento = request.POST.get('id_evento')
+        # Se já existe o id_evento eu altero, senão crio um novo evento.
         if id_evento:
-            evento = Evento.objects.get('id_evento')
+            evento = Evento.objects.get(id=id_evento)
             if evento.usuario == usuario:
                 evento.titulo = titulo
                 evento.descricao = descricao
                 evento.data_evento = data_evento
                 evento.save()
-            #Update - sem validação de Usuário
-            #Evento.objects.filter(id=id_evento).update(titulo=titulo,
-            #                                           data_evento=data_evento,
-            #                                           descricao=descricao)
+            # Update - sem validação de Usuário
+            # Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                            data_evento=data_evento,
+            #                                            descricao=descricao)
         else:
             Evento.objects.create(titulo=titulo,
                                   data_evento=data_evento,
                                   descricao=descricao,
                                   usuario=usuario)
+
     return redirect('/')
+
 
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento.usuario:
         evento.delete()
+    else:
+        raise Http404()
     return redirect('/')
 
 
-
+@login_required(login_url='/login/')
+def json_lista_evento(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
 
 
